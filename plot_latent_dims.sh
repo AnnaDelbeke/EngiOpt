c40c7049d5e0b61baed6fcc982cbfae6027acd32
Anna Adelbeke <adelbeke@ethz.ch>
2026-06-26 16:29:19 +0200

    delete unused files

--- plot_latent_dims.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=plot_latent_dims
-#SBATCH --output=logs/plot_latent_dims_%j.out
-#SBATCH --time=00:30:00
-#SBATCH --mem-per-cpu=32G
-
-source /cluster/home/adelbeke/EngiOpt/.venv/bin/activate
-cd /cluster/home/adelbeke/EngiOpt
-python -u plot_latent_dims.py
